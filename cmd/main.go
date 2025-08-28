178e9e0014399b0b5a63d2243573af9ba23776d1
Dhairya3124 <dk9582671597@gmail.com>
2025-08-28 07:34:47 +0530

    feat: added repository for coupon functions

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,14 @@ import (
 	"log"
 
 	"github.com/Dhairya3124/e-commerce-coupon-system/internal/db"
+	"github.com/Dhairya3124/e-commerce-coupon-system/internal/repository"
 )
 
 func main() {
 	dbConn := db.NewDB()
-	fmt.Println(dbConn, "done")
+	repo := repository.NewCouponRepository(dbConn.DB)
+
+	fmt.Println(dbConn, repo, "done")
 	log.Fatal(dbConn)
 
 }
---
+++ internal/repository/repository.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"context"
+	"sync"
+
+	"github.com/Dhairya3124/e-commerce-coupon-system/internal/model"
+	"gorm.io/gorm"
+)
+
+type Coupon interface {
+	Create(ctx context.Context, coupon *model.Coupon) error
+	GetCouponByCode(ctx context.Context, code string) (*model.Coupon, error)
+	UpdateCoupon(ctx context.Context, coupon *model.Coupon) error
+	GetAllCoupons(ctx context.Context) ([]*model.Coupon, error)
+}
+
+type couponStore struct {
+	db *gorm.DB
+	mu *sync.RWMutex
+}
+
+func NewCouponRepository(db *gorm.DB) Coupon {
+	return &couponStore{db: db, mu: &sync.RWMutex{}}
+}
+func (c *couponStore) Create(ctx context.Context, coupon *model.Coupon) error {
+	if err := c.db.WithContext(ctx).Create(coupon).Error; err != nil {
+		return err
+
+	}
+
+	return nil
+}
+
+func (c *couponStore) GetCouponByCode(ctx context.Context, code string) (*model.Coupon, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var coupon model.Coupon
+	if err := c.db.WithContext(ctx).Where("code = ?", code).First(&coupon).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &coupon, nil
+}
+func (c *couponStore) UpdateCoupon(ctx context.Context, coupon *model.Coupon) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.db.WithContext(ctx).Save(coupon).Error
+}
+func (c *couponStore) GetAllCoupons(ctx context.Context) ([]*model.Coupon, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var coupons []*model.Coupon
+	if err := c.db.WithContext(ctx).Find(&coupons).Error; err != nil {
+		return nil, err
+	}
+	return coupons, nil
+}
